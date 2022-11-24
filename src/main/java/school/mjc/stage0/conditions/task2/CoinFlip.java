7063ece3d4d9413076092152ac86bcec4786d94b
diyorjon090 <saidovdiyorbek090@gmail.com>
2022-11-24 12:42:07 +0500

    module4.1 task2

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,11 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-        if (from1UpTo1000 < 500) {
-            System.out.println("Eagle");
-        }
-        else {
-            System.out.println("Tail");
-        }
+       String result = from1UpTo1000 < 500 ? "Eagle" : "Tail";
+        System.out.println(result);
     }
 }
